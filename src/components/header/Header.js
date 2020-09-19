aa4bb2b4b22765992e4eebc3d57a0bc25cc147c5
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-09-19 18:11:52 +0400

    added sideNav Component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavbarMobile from '../nav/navbar-mobile/NavbarMobile';
 import NavbarDesktop from '../nav/navbar-desktop/NavbarDesktop';
-import SidenavMobile from '../nav/sidenav-mobile/sidenavMobile';
+import SidenavMobile from '../nav/sidenav-mobile/SidenavMobile';
 export default function Header() {
   return (
     <header>
