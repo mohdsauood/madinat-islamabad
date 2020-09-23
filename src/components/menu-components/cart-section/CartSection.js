593863c69a26867b4366daf33f9d8ec17a9a293b
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-09-24 03:18:43 +0400

    feat&bug add dynamic desk cartsection bug:101x
    -added usecart state in cartsection
    -bug no sync when changing states like background not updating and text is not changing from good food is waiting add items now

--- src/components/menu-components/cart-section/CartSection.js
+++ src/components/menu-components/cart-section/CartSection.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import styles from './CartSection.module.css';
+import { useCartState } from '../../../context/cart-provider-context/cart-provider-context';
 
 export default function CartSection() {
-  return (
+  const cartState = useCartState();
+
+  return generateCart(cartState);
+}
+
+function generateCart(cart) {
+  let cartValues;
+  let bgImg;
+  if (cart.length == 0) {
+    cartValues = { items: [] };
+    bgImg = { backgroundImage: `url('/assets/cooking.JPG')` };
+  } else {
+    cartValues = { items: [] };
+    cart.forEach((element) => {
+      cartValues.items.push(element.name);
+    });
+    bgImg = { backgroundImage: `url('/assets/cart.JPG'` };
+  }
+
+  const emptyCart = (
     <section className={styles.cartSection}>
-      <h2 className={styles.cartSection__h2}>CART EMPTY</h2>
-      <div className={styles.cartSection__div}></div>
+      <h2
+        className={
+          styles.cartSection__h2
+        }>{`${cartValues.items.length} items`}</h2>
+      <div className={styles.cartSection__div} style={bgImg}></div>
       <p className={styles.cartSection__p}>
-        Good food is always cooking ! go ahead , order some yummy items from the
-        menu
+        {cartValues.items.map((item) => (
+          <span>{item}-</span>
+        ))}
+        {cartValues.length == 0 && 'good foor is waiting'}
       </p>
     </section>
   );
+
+  return emptyCart;
 }
