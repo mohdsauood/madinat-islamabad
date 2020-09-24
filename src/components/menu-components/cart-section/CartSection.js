10b87bc7907c947fc719a7d495b02e7bd421f2a4
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-09-25 03:12:50 +0400

    styles refactored cartsection css and sync cartState

--- src/components/menu-components/cart-section/CartSection.js
+++ src/components/menu-components/cart-section/CartSection.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import styles from './CartSection.module.css';
 import { useCartState } from '../../../context/cart-provider-context/cart-provider-context';
-import { v4 as uuidv4 } from 'uuid';
 
 export default function CartSection() {
   const cartState = useCartState();
+  let cartValues, bgImg, totalItems, totalPrice;
 
-  return generateCart(cartState);
-}
-
-function generateCart(cart) {
-  let cartValues;
-  let bgImg;
-  if (cart.length == 0) {
+  if (cartState.length == 0) {
+    totalItems = 0;
     cartValues = { items: [] };
     bgImg = { backgroundImage: `url('/assets/cart.JPG')` };
+    totalPrice = ` Good food is always cooking ! go ahead , order some yummy items from
+    the menu`;
   } else {
     cartValues = { items: [] };
-    cart.forEach((element) => {
+    cartState.forEach((element) => {
       cartValues.items.push(element.name);
     });
+    totalItems = cartState.reduce((accum, fooditem) => {
+      return accum + fooditem.quantity;
+    }, 0);
+    totalPrice = cartState.reduce((accum, fooditem) => {
+      return accum + parseInt(fooditem.price) * fooditem.quantity;
+    }, 0);
+    totalPrice = `Total Price: ${totalPrice} AED`;
     bgImg = { backgroundImage: `url('/assets/cooking.JPG'` };
   }
 
-  const emptyCart = (
+  return (
     <section className={styles.cartSection}>
       <h2 className={styles.cartSection__h2}>
-        {cartValues.items.length == 0
+        {totalItems == 0
           ? 'Cart Empty'
-          : `${cartValues.items.length} items`}
+          : totalItems == 1
+          ? '1 item'
+          : `${totalItems} items`}
       </h2>
       <div className={styles.cartSection__div} style={bgImg}></div>
-      <p className={styles.cartSection__p}>
-        {cartValues.items.length == 0
-          ? ` Good food is always cooking ! go ahead , order some yummy items from
-          the menu`
-          : cartValues.items.map((item) => <span key={uuidv4()}>{item}-</span>)}
-      </p>
+      <p className={styles.cartSection__p}>{totalPrice}</p>
     </section>
   );
-  return emptyCart;
 }
