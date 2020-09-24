1ae83404419a540bf1c08e7a4c29914373ba858d
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-09-25 02:18:38 +0400

    feat viewcart button is in sync with state and shows proper total items and price

--- src/components/menu-components/viewcart-button/ViewCartButton.js
+++ src/components/menu-components/viewcart-button/ViewCartButton.js
@@ -4,11 +4,20 @@ import styles from './ViewCartButton.module.css';
 import { useCartState } from '../../../context/cart-provider-context/cart-provider-context';
 export default function ViewCartButton() {
   const cartState = useCartState();
+  let totalItems = cartState.reduce((accum, fooditem) => {
+    return accum + fooditem.quantity;
+  }, 0);
+  totalItems = totalItems > 1 ? `${totalItems} items` : `${totalItems} item`;
+  let totalPrice = cartState.reduce((accum, fooditem) => {
+    return accum + parseInt(fooditem.price) * fooditem.quantity;
+  }, 0);
   return (
     cartState.length > 0 && (
       <button
         className={`${styles.viewCartBtn} btn btnPrimary btnGreen ${styles.show}`}>
-        <p className={styles.viewCartBtn__price}>1 item | 4 AED</p>
+        <p className={styles.viewCartBtn__price}>
+          {totalItems} | {totalPrice} AED
+        </p>
         <p className={styles.viewCartBtn__cart}>View cart</p>
       </button>
     )
