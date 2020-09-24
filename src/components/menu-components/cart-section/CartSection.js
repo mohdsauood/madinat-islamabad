00c4c4de2a79245c756c15135956a1646cf3c8a4
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-09-24 15:29:21 +0400

    fix bug101x refactored code to sync in component CartSection

--- src/components/menu-components/cart-section/CartSection.js
+++ src/components/menu-components/cart-section/CartSection.js
@@ -13,30 +13,30 @@ function generateCart(cart) {
   let bgImg;
   if (cart.length == 0) {
     cartValues = { items: [] };
-    bgImg = { backgroundImage: `url('/assets/cooking.JPG')` };
+    bgImg = { backgroundImage: `url('/assets/cart.JPG')` };
   } else {
     cartValues = { items: [] };
     cart.forEach((element) => {
       cartValues.items.push(element.name);
     });
-    bgImg = { backgroundImage: `url('/assets/cart.JPG'` };
+    bgImg = { backgroundImage: `url('/assets/cooking.JPG'` };
   }
 
   const emptyCart = (
     <section className={styles.cartSection}>
-      <h2
-        className={
-          styles.cartSection__h2
-        }>{`${cartValues.items.length} items`}</h2>
+      <h2 className={styles.cartSection__h2}>
+        {cartValues.items.length == 0
+          ? 'Cart Empty'
+          : `${cartValues.items.length} items`}
+      </h2>
       <div className={styles.cartSection__div} style={bgImg}></div>
       <p className={styles.cartSection__p}>
-        {cartValues.items.map((item) => (
-          <span>{item}-</span>
-        ))}
-        {cartValues.length == 0 && 'good foor is waiting'}
+        {cartValues.items.length == 0
+          ? ` Good food is always cooking ! go ahead , order some yummy items from
+          the menu`
+          : cartValues.items.map((item) => <span>{item}-</span>)}
       </p>
     </section>
   );
-
   return emptyCart;
 }
