27467a4876da1661375d9be094a7095e39931d8b
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-10-05 00:22:40 +0400

    feat add responsiveness

--- src/components/cart/apply-coupon/ApplyCoupon.js
+++ src/components/cart/apply-coupon/ApplyCoupon.js
@@ -12,7 +12,7 @@ export default function ApplyCoupon() {
     <>
       <div onClick={handleClick} className={styles.div}>
         <svg
-          className="svgGray"
+          className={`${styles.svg} svgGray`}
           width="24"
           height="24"
           xmlns="http://www.w3.org/2000/svg"
