756004e6d285c79571a893252877556937951b1d
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-10-05 15:58:39 +0400

    feat add routing in cart page
    when cart items are zero then redirect to menu page

--- src/components/cart/cart-items/CartItems.js
+++ src/components/cart/cart-items/CartItems.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './CartItems.module.css';
 import CartItem from '../cart-item/CartItem';
+import { useRouter } from 'next/router';
 
 import { useCart } from '../../../context/cart-provider-context/cart-provider-context';
 export default function CartItems() {
+  const router = useRouter();
   const [cartState, cartDispatch] = useCart();
+  if (cartState.length == 0) {
+    router.push('/menu/roti');
+  }
   let items = cartState.map((item) => <CartItem item={item} />);
   return <div className={styles.itemsDiv}>{items}</div>;
 }
