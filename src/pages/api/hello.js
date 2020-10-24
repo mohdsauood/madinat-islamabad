7dc83610727b82ab98a11c32c282ff237648640f
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-10-25 02:05:45 +0400

    feat init custom api for testing db error

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dbConnect from '../../utils/dbConnect';
+import User from '../../models/User';
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  await dbConnect();
+  const name = 'SALMAN KHAN';
+  const email = 'swampfire.saud@gmail.com';
+  const restoUser = new User({ name, email });
+  User.findOrCreate({ email }, restoUser, function (err, user) {
+    if (err) return console.error(err);
+    // user.id=restoUser._id;
+    res.statusCode = 200;
+    res.json({ user });
+  });
+};
