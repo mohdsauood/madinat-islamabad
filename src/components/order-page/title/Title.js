e919cbc5e46772d0c536c4bdb4139e5821a7cca0
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-11-04 17:07:07 +0400

    feat init date formatting

--- src/components/order-page/title/Title.js
+++ src/components/order-page/title/Title.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import styles from './Title.module.css';
+import { useCartState } from '../../../context/cart-provider-context/cart-provider-context';
 export default function Title() {
+  const cartState = useCartState();
+  const { user } = cartState;
+  const order = user.orders && user.orders[user.orders.length - 1];
   return (
     <Card className="ml-3 mr-3">
       <Card.Body className="p-2">
@@ -14,10 +18,11 @@ export default function Title() {
             viewBox="0 0 24 24">
             <path d="M22.548 9l.452-2h-5.364l1.364-6h-2l-1.364 6h-5l1.364-6h-2l-1.364 6h-6.184l-.452 2h6.182l-1.364 6h-5.36l-.458 2h5.364l-1.364 6h2l1.364-6h5l-1.364 6h2l1.364-6h6.185l.451-2h-6.182l1.364-6h5.366zm-8.73 6h-5l1.364-6h5l-1.364 6z" />
           </svg>
-          ORDER 6387902111
+          ORDER {order && order._id}
         </Card.Title>
         <Card.Subtitle className="mb-2 text-muted d-flex justify-content-between">
-          2 items | 30 AED
+          Items : {order && order.items.length} | {order && order.bill.toPay}{' '}
+          AED
           <span className={styles.help}>HELP</span>
         </Card.Subtitle>
       </Card.Body>
--- src/pages/order-success/index.js
+++ src/pages/order-success/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './index.module.css';
 import Header from '../../components/header/Header';
 import Title from '../../components/order-page/title/Title';
@@ -6,7 +6,24 @@ import Address from '../../components/order-page/address/Address';
 import Date from '../../components/order-page/date/Date';
 import Bill from '../../components/order-page/bill/Bill';
 import Overlay from '../../components/overlay/Overlay';
+import { useCartDispatch } from '../../context/cart-provider-context/cart-provider-context';
+import { useSession } from 'next-auth/client';
+import getUserType from '../../utils/getUserType';
 export default function index() {
+  const [session] = useSession();
+  const cartDispatch = useCartDispatch();
+  useEffect(() => {
+    async function fetchUser() {
+      if (session && session.user) {
+        const userProperties = Object.keys(session.user);
+        userProperties.forEach((property) => {
+          const type = getUserType(property);
+          cartDispatch({ type, payload: session.user[property] });
+        });
+      }
+    }
+    fetchUser();
+  }, [session]);
   return (
     <>
       <Overlay />
