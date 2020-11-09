32fa0cd94fada045af3e5471abf467cf5c09763b
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-11-09 23:23:45 +0400

    feat marker checking if inside circle

--- src/pages/address/index.js
+++ src/pages/address/index.js
@@ -18,8 +18,8 @@ import { Circle } from '@react-google-maps/api';
 
 const libraries = ['places'];
 const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh',
+  width: '100%',
+  height: '40vh',
 };
 const center = {
   lat: 25.33800452203996,
@@ -35,24 +35,25 @@ const options = {
   draggable: false,
   editable: false,
   visible: true,
-  radius: 1000,
+  radius: 1050,
   zIndex: 1,
 };
+
+const initialMarker = { lat: null, long: null };
 
 export default function index() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     libraries,
   });
-
-  const [marker, setMarker] = useState({ lat: null, long: null });
+  const [marker, setMarker] = useState(initialMarker);
   const [showModal, setShowModal] = useState(false);
   if (loadError) return 'Error Loading Maps';
   if (!isLoaded) return 'Loading Maps';
 
   const handleModalClose = () => {
     setShowModal(false);
-    setMarker(null);
+    setMarker(initialMarker);
   };
 
   return (
@@ -60,35 +61,18 @@ export default function index() {
       <div>
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
-          zoom={15}
+          zoom={14}
           center={center}
           onClick={(event) => {
             setMarker({
               lat: event.latLng.lat(),
               lng: event.latLng.lng(),
             });
-            // if (
-            //   getDistanceFromLatLonInKm(
-            //     process.env.NEXT_PUBLIC_RESTO_LAT,
-            //     process.env.NEXT_PUBLIC_RESTO_LONG,
-            //     marker.lat,
-            //     marker.lng
-            //   ) > 1
-            // ) {
-            //   setShowModal(true);
-            // }
-            console.log(
-              google.maps.geometry.spherical.computeDistanceBetween(
-                new google.maps.LatLng(center.lat, center.lng),
-                new google.maps.LatLng(marker.lat, marker.lng),
-                1000
-              ) * 1000
-            );
+
             if (
               google.maps.geometry.spherical.computeDistanceBetween(
-                { lat: center.lat, lng: center.lng },
-                { lat: marker.lat, lng: marker.lng },
-                1000
+                new google.maps.LatLng(center.lat, center.lng),
+                new google.maps.LatLng(marker.lat, marker.lng)
               ) > 1000
             ) {
               setShowModal(true);
