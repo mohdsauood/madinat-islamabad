37fd530cf12b767b16c17a70bc9f6ba984e84088
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-11-09 15:46:20 +0400

    feat init modal but error of rendering too many times

--- src/pages/address/index.js
+++ src/pages/address/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
+  MarkerClusterer,
 } from '@react-google-maps/api';
 import '@reach/combobox/styles.css';
 import usePlacesAutoComplete, {
   getGeocode,
   getLatLng,
 } from 'use-places-autocomplete';
+import getDistanceFromLatLonInKm from '../../utils/getDistanceFromLatLonInKm.js';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 const libraries = ['places'];
 const mapContainerStyle = {
@@ -22,18 +26,63 @@ const center = {
 };
 export default function index() {
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     libraries,
   });
 
+  const [marker, setMarker] = useState({ lat: null, long: null });
+  const [showModal, setShowModal] = useState(false);
   if (loadError) return 'Error Loading Maps';
   if (!isLoaded) return 'Loading Maps';
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    setMarker(null);
+  };
+  // useEffect(() => {
+  //   if (
+  //     marker &&
+  //     getDistanceFromLatLonInKm(
+  //       process.env.NEXT_PUBLIC_RESTO_LAT,
+  //       process.env.NEXT_PUBLIC_RESTO_LONG,
+  //       marker.lat,
+  //       marker.lng
+  //     ) > 3
+  //   ) {
+  //     setShowModal(true);
+  //   }
+  // }, [marker]);
   return (
-    <div>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={18}
-        center={center}></GoogleMap>
-    </div>
+    <>
+      <div>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={14}
+          center={center}
+          onClick={(event) => {
+            setMarker({
+              lat: event.latLng.lat(),
+              lng: event.latLng.lng(),
+            });
+          }}>
+          {marker && <Marker position={{ lat: marker.lat, lng: marker.lng }} />}
+        </GoogleMap>
+      </div>
+      <Modal
+        show={showModal}
+        onHide={handleModalClose}
+        backdrop="static"
+        keyboard={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Address is out of bounds</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Sorry ! We Dont Deliver Food In Your Area .</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleModalClose}>
+            Choose New Address
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
