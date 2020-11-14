1a03a1b2da3be2bce9e0a3d51109646eafcbda56
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-11-14 19:39:18 +0400

    refactor attempt for default radio button

--- src/components/address-page/address-fields/AddressFields.js
+++ src/components/address-page/address-fields/AddressFields.js
@@ -37,7 +37,7 @@ export default function AddressFields({ geoLocation }) {
             });
         }}
         initialValues={{
-          name: '',
+          name: 'home',
           area: '',
           street: '',
           doorNo: '',
