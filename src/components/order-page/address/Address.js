8de031a28570f8f1eb115441628402ec528a3b23
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-11-17 16:32:00 +0400

    fix order not defined error

--- src/components/order-page/address/Address.js
+++ src/components/order-page/address/Address.js
@@ -27,7 +27,7 @@ export default function Address({ id }) {
             viewBox="0 0 24 24">
             <path d="M12 2c3.196 0 6 2.618 6 5.602 0 3.093-2.493 7.132-6 12.661-3.507-5.529-6-9.568-6-12.661 0-2.984 2.804-5.602 6-5.602m0-2c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
           </svg>
-          {specificOrder ? specificOrder.name : order.address.name}
+          {specificOrder ? specificOrder.name : order?.address.name}
         </Card.Title>
         <Card.Subtitle className="mb-2 text-muted d-flex justify-content-between">
           {specificOrder
@@ -43,12 +43,12 @@ export default function Address({ id }) {
               ? specificOrder.address.landmark
               : ''
           } ${specificOrder.address.city}`
-            : `${order.address.doorNo} , ${order.address.area} , ${
-                order.address.street !== undefined ? order.address.street : ''
+            : `${order?.address.doorNo} , ${order?.address.area} , ${
+                order?.address.street !== undefined ? order?.address.street : ''
               }
           ${
-            order.address.landmark !== undefined ? order.address.landmark : ''
-          } ${order.address.city}`}
+            order?.address.landmark !== undefined ? order?.address.landmark : ''
+          } ${order?.address.city}`}
         </Card.Subtitle>
       </Card.Body>
     </Card>
