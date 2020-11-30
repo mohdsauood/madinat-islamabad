d03fb431164ec4e708b4311f4684b5f02d7d10b8
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-11-30 21:30:08 +0400

    feat init top-products section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from '../styles/index.module.css';
 import Overlay from '../components/overlay/Overlay';
 import Header from '../components/header/Header';
