5f1d5adb7947b9582608130e618a734d5adb43d1
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-12-08 17:17:04 +0400

    fix server returning proper food time by using moment tz

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -18,16 +18,6 @@ function checkTime(beforeTime, afterTime) {
   const format = 'HH:mm';
   const bt = moment.tz(beforeTime, format, 'Asia/Dubai');
   const at = moment.tz(afterTime, format, 'Asia/Dubai');
-  // const bt = moment(beforeTime, format);
-  // const at = moment(afterTime, format);
-  // console.log('before time ' + bt.format('h mm a DD'));
-  // console.log('after time ' + at.format('h mm a DD'));
-  // console.log('printing server time ' + moment().format('h mm a DD'));
-
-  /*server time is 4 hours behind dubai time */
-  if (process.env.SITE_URL == 'http://localhost:3000/') {
-    return moment.tz('Asia/Dubai').isBetween(bt, at);
-  }
   return moment.tz('Asia/Dubai').isBetween(bt, at);
 }
 
