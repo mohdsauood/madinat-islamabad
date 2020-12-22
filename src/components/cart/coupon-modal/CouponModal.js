572704f67925d9a423c589e377f6d063adde1e1b
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-12-22 15:05:04 +0400

    refactor apply coupon modal to not accept coupons on cart page

--- src/components/cart/coupon-modal/CouponModal.js
+++ src/components/cart/coupon-modal/CouponModal.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
 import styles from './CouponModal.module.css';
 import { useCartPageUi } from '../../../context/cart-page-ui-context/cart-page-ui-context';
 import { useCart } from '../../../context/cart-provider-context/cart-provider-context';
@@ -18,38 +16,20 @@ export default function CouponModal() {
   const handleClose = () => {
     uiDispatch({ type: HIDE_COUPON_MODAL });
   };
-  const handleSubmit = () => {
-    uiDispatch({ type: HIDE_COUPON_MODAL });
-    cartDispatch({ type: CLEAR_COUPON });
-  };
-  const handleInput = (e) => {
-    cartDispatch({ type: UPDATE_COUPON, payload: e.target.value });
-  };
   return (
     <Modal centered show={uiState.showCouponModal} onHide={handleClose}>
       <Modal.Header className={styles.header} closeButton>
         <Modal.Title className={styles.title}>
-          Apply A Coupon On This Order
+          sorry no coupons applicable currently
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <InputGroup className="mb-3">
-          <FormControl
-            value={cartState.bill.coupon}
-            className={styles.input}
-            onChange={handleInput}
-            placeholder="Enter Coupon Code"
-            aria-label="couponCode"
-            aria-describedby="basic-addon1"
-          />
-        </InputGroup>
-      </Modal.Body>
+      <Modal.Body></Modal.Body>
       <Modal.Footer className={styles.footer}>
         <Button
           className={`${styles.btn} d-block w-100`}
           variant="primary"
-          onClick={handleSubmit}>
-          use Coupon
+          onClick={handleClose}>
+          ok
         </Button>
       </Modal.Footer>
     </Modal>
